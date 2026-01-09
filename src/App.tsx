414fb81fde648acb38aec7b9e153f6eb206a43f5
punadev <60507424+punadev@users.noreply.github.com>
2026-01-09 18:33:32 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Impact from './components/Impact';
 import Benefits from './components/Benefits';
 import AboutAuthor from './components/AboutAuthor';
 import CTA from './components/CTA';
-import CTA from './components/Footer';
+import Footer from './components/Footer';
 
 function App() {
   return (
